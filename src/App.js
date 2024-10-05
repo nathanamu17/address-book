724fc80c554e6dcfd9176b67b1663cb25ea1c012
nathanamu17 <nathanamu17@gmail.com>
2024-10-05 09:21:38 -0300

    add frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddressList from "./components/AddressList";
+import AddAddress from "./components/AddAddress";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container container-fluid min-vh-100 d-flex flex-column">
+        <Routes>
+          <Route exact path="/" element={<AddressList />} />
+          <Route exact path="/add" element={<AddAddress />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
